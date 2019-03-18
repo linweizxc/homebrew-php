49f9c306bf6c909c7cdb3e0ea599fda70cd50b0d
林维 <linweizxc@126.com>
2019-03-19 02:47:59 +0800

    5.6.40

--- Formula/php@5.6.rb
+++ Formula/php@5.6.rb
@@ -134,7 +134,7 @@ class PhpAT56 < Formula
       --with-gd
       --with-gettext=#{Formula["gettext"].opt_prefix}
       --with-gmp=#{Formula["gmp"].opt_prefix}
-      --with-iconv#Formula["libiconv"]/1.15/lib
+      --with-iconv#{Formula["libiconv"]}/1.15/lib
       --with-icu-dir=#{Formula["icu4c"].opt_prefix}
       --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
       --with-kerberos#{headers_path}
